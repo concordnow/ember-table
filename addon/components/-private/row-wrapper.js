4c930019361897ec43fffe966d76241dd15cd3a3
frykten <fry.kten.art@gmail.com>
2019-11-27 15:16:03 +0100

    [FEAT] Enable row toggling with body property

--- addon/components/-private/row-wrapper.js
+++ addon/components/-private/row-wrapper.js
@@ -34,6 +34,7 @@ export default Component.extend({
   columns: undefined,
   rowMetaCache: undefined,
   rowSelectionMode: undefined,
+  rowToggleMode: undefined,
   rowValue: undefined,
 
   init() {
@@ -48,15 +49,24 @@ export default Component.extend({
     this._super(...arguments);
   },
 
-  api: computed('rowValue', 'rowMeta', 'cells', 'canSelect', 'rowSelectionMode', function() {
-    let rowValue = this.get('rowValue');
-    let rowMeta = this.get('rowMeta');
-    let cells = this.get('cells');
-    let canSelect = this.get('canSelect');
-    let rowSelectionMode = canSelect ? this.get('rowSelectionMode') : 'none';
+  api: computed(
+    'rowValue',
+    'rowMeta',
+    'cells',
+    'canSelect',
+    'rowSelectionMode',
+    'rowToggleMode',
+    function() {
+      let rowValue = this.get('rowValue');
+      let rowMeta = this.get('rowMeta');
+      let cells = this.get('cells');
+      let canSelect = this.get('canSelect');
+      let rowSelectionMode = canSelect ? this.get('rowSelectionMode') : 'none';
+      let rowToggleMode = this.get('rowToggleMode');
 
-    return { rowValue, rowMeta, cells, rowSelectionMode };
-  }),
+      return { rowValue, rowMeta, cells, rowSelectionMode, rowToggleMode };
+    }
+  ),
 
   rowMeta: computed('rowValue', function() {
     let rowValue = this.get('rowValue');
